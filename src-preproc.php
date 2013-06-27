b4480bbb71d8a2c04db2507eec73396707b9f502
Vitaliy <fornit1917@gmail.com>
2013-06-27 22:39:37 +0400

    correct handling of errors in recursive calls of preprocFile

--- src-preproc.php
+++ src-preproc.php
@@ -15,23 +15,24 @@ else {
 	$constants = explode(',', $argv[2]);
 }
 
-$fout = fopen($outName, 'w');
+$fout = @fopen($outName, 'w');
 if ($fout === false) {
 	die("ERROR: can not open the file \"$outName\" for writing \r\n");
 }
 
 $ok = preprocFile($inName, $constants, $fout);
+
 fclose($fout);
 if (!$ok && is_file($outName)) {
 	unlink($outName);
 }
-else {
+elseif($ok) {
 	echo "Preprocessing completed successfully\r\n";
 }
 
 function preprocFile($inName, $constants, $fout)
 {
-	$fin = fopen($inName, 'r');
+	$fin = @fopen($inName, 'r');
 	if ($fin === false) {
 		echo "ERROR: can not open the file \"$inName\" for reading \r\n";
 		return false;
@@ -39,18 +40,31 @@ function preprocFile($inName, $constants, $fout)
 	
 	$preWd = getcwd();
 	chdir(dirname($inName));
+	
+	$i = 0;
 	while ($s = fgets($fin)) {
-
+		
+		$i++;
+		//it's #include
 		preg_match('!^\s*(#|\/\/|\/\*)#include\s+(.*?)(\*\/)?\s*$!', $s, $matches);
 		if ($matches && ($matches[1] != '/*' || isset($matches[3]))) {
 			$includeName = trim($matches[2]);
-			$ok = preprocFile(realpath($includeName), $constants, $fout);
+			$includeFullName = realpath($includeName);
+			if($includeFullName === false) {
+				echo "ERROR in \"".  realpath($inName)."\", line $i: file \"$includeName\" not exists \r\n";
+				chdir($preWd);
+				return false;
+			}
+			
+			$ok = preprocFile($includeName, $constants, $fout);
 			if (! $ok) {
+				chdir($preWd);
 				return false;
 			}
 			continue;
 		}	
 		
+		//it's not command for preprocessor
 		fputs($fout, $s);
 		if (substr($s, -1) !== "\n") {
 			fputs($fout, "\r\n");
